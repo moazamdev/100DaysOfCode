fc5fc88697c93c7c85ff8745d8578139046a8ad5
Moazam Ali <mozalikorai@gmail.com>
2023-09-05 19:14:01 -0700

    removed component

--- src/components/day25/TaskTracker.jsx
+++ src/components/day25/TaskTracker.jsx
@@ -1,14 +1,19 @@
-import MyColumn from "./column";
+import React from "react";
+// import MyColumn from "./column";
+import { Helmet } from "react-helmet";
 
 const TaskTracker = () => {
 	return (
 		<>
+			<Helmet>
+				<title>Task Tracker</title>
+			</Helmet>
 			<div style={{ display: "flex", justifyContent: "center" }}>
 				<h1>Task Tracker</h1>
 			</div>
-			<MyColumn state={"PLANNED"} />
+			{/* <MyColumn state={"PLANNED"} />
 			<MyColumn state={"ONGOING"} />
-			<MyColumn state={"DONE"} />
+			<MyColumn state={"DONE"} /> */}
 		</>
 	);
 };
