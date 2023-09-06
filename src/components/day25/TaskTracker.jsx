65009c559e9577400fde780550db832045642b72
Moazam Ali <mozalikorai@gmail.com>
2023-09-05 19:16:29 -0700

    added component

--- src/components/day25/TaskTracker.jsx
+++ src/components/day25/TaskTracker.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import MyColumn from "./column";
+import MyColumn from "./column";
 import { Helmet } from "react-helmet";
 
 const TaskTracker = () => {
@@ -11,9 +11,9 @@ const TaskTracker = () => {
 			<div style={{ display: "flex", justifyContent: "center" }}>
 				<h1>Task Tracker</h1>
 			</div>
-			{/* <MyColumn state={"PLANNED"} />
+			<MyColumn state={"PLANNED"} />
 			<MyColumn state={"ONGOING"} />
-			<MyColumn state={"DONE"} /> */}
+			<MyColumn state={"DONE"} />
 		</>
 	);
 };
